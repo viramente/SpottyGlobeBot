ebc94c419380e932349e65f934115870d6cffe23
viramente <viramente@gmail.com>
2024-01-04 17:50:32 +0200

    SGBot_Stage04 simplifying "answers" creation, NumberFormatException catching

--- src/main/kotlin/ConsoleTrainer.kt
+++ src/main/kotlin/ConsoleTrainer.kt
@@ -2,30 +2,27 @@ import java.io.File
 
 fun main() {
 
-    val dictionarySource = File("words.txt")
+    if (!File("words.txt").exists()) createInitialDictionary()
 
-    dictionarySource.writeText(
-        """
-        hello|привет^0
-        dog|собака^3
-        cat|кошка^3
-        ball|мяч^1
-        girl|девочка^3
-        house|дом^3
-        pen|ручка^1
-        door|дверь^3
-   """.trimIndent()
-    )
+    val dictionarySource = File("words.txt")
 
     val dictionary = mutableListOf<Word>()
 
     dictionarySource.forEachLine {
+
         val stringToParse = it.split("|", "^")
+
+        val rightAnswersAmount = try {
+            stringToParse[2].toInt()
+        } catch (e: NumberFormatException) {
+            0
+        }
+
         dictionary.add(
             Word(
                 stringToParse[0],
                 stringToParse[1],
-                stringToParse[2].toInt() ?: 0
+                rightAnswersAmount
             )
         )
     }
@@ -45,30 +42,20 @@ fun main() {
                     break
                 } else {
 
-                    val taskWord = unlearnedWords.random()
+                    val answers = unlearnedWords.shuffled().take(POSED_ANSWERS_AMOUNT).toMutableList()
 
-                    val wrongAnswers = (unlearnedWords - taskWord).toMutableList()
+                    val taskWord = answers.random()
 
-                    if (wrongAnswers.size < POSED_ANSWERS_AMOUNT - 1) {
-                        val extraWrongAnswers = (dictionary - taskWord - wrongAnswers)
+                    if (answers.size < POSED_ANSWERS_AMOUNT) {
+                        val extraWrongAnswers = (dictionary - answers)
                             .shuffled()
-                            .take(POSED_ANSWERS_AMOUNT - wrongAnswers.size)
-                        wrongAnswers.addAll(extraWrongAnswers)
+                            .take(POSED_ANSWERS_AMOUNT - answers.size)
+                        answers.addAll(extraWrongAnswers)
                     }
 
-                    val answers = mutableListOf<String>()
-                    answers.add(taskWord.translate)
-                    answers.addAll(wrongAnswers.take(POSED_ANSWERS_AMOUNT - 1).map { it.translate })
-
                     println("Выберете перевод для слова: ${taskWord.original}")
-                    println(
-                        """
-                        1 ${answers[0]}
-                        2 ${answers[1]}
-                        3 ${answers[2]}
-                        4 ${answers[3]}
-                        """.trimIndent()
-                    )
+
+                    answers.forEach { println("${answers.indexOf(it) + 1} ${it.translate}") }
                 }
 
                 continue
@@ -100,4 +87,26 @@ const val MATCHES_AMOUNT_TO_BECOME_LEARNED = 3
 
 fun getPercents(whole: Int, part: Int) = (100 * part) / whole
 
-data class Word(val original: String, val translate: String, var correctAnswersCount: Int = 0)
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0
+)
+
+fun createInitialDictionary() {
+    val dictionarySource = File("words.txt")
+
+    dictionarySource.writeText(
+        """
+        hello|привет^0
+        dog|собака^3
+        cat|кошка^3
+        ball|мяч^1
+        girl|девочка^3
+        house|дом^3
+        pen|ручка^1
+        door|дверь^3
+   """.trimIndent()
+    )
+
+}
